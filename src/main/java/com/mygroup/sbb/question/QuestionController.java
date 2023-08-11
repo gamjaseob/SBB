8acd041b46f83e84ea6193ef9a48749b8a7cf024
Jiseob Kim <rkawkg123@gmail.com>
2023-08-11 17:12:09 +0900

    update

--- src/main/java/com/mygroup/sbb/question/QuestionController.java
+++ src/main/java/com/mygroup/sbb/question/QuestionController.java
@@ -23,6 +23,8 @@ import org.springframework.validation.BindingResult;
 
 import lombok.RequiredArgsConstructor;
 
+import com.mygroup.sbb.chatgpt.ChatGptMessageService;
+
 @RequestMapping("/question")
 @RequiredArgsConstructor
 @Controller
@@ -30,6 +32,7 @@ public class QuestionController {
 
     private final QuestionService questionService;
     private final UserService userService;
+    private final ChatGptMessageService chatGptMessageService;
 
     @GetMapping("/list")
     public String list(Model model, @RequestParam(value="page", defaultValue="0") int page) {
@@ -58,7 +61,8 @@ public class QuestionController {
             return "question_form";
         }
         SiteUser siteUser = this.userService.getUser(principal.getName());
-        this.questionService.create(questionForm.getSubject(), questionForm.getContent(), siteUser);
+        Question question = this.questionService.create(questionForm.getSubject(), questionForm.getContent(), siteUser);
+        this.chatGptMessageService.sendMessage(question);
         return "redirect:/question/list"; // 질문 저장후 질문목록으로 이동
     }
 
